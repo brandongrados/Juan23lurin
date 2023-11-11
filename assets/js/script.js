7fc8c14f37b2d6f3975b2f8ff79b5ab8590c9caf
BRANDONGRADOS <bgradosalvarez@gmail.com>
2023-11-10 19:51:44 -0500

    version 1.0 finalizada

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//const cors = require('cors');
+
+//document.use(cors())
+
+const evento = document.getElementById('send');
+const enviarFormulario =() =>{
+    let nombre = document.getElementById('nombres').value;
+    //let apellido = document.getElementById('apellidos').value;
+    let mensaje = document.getElementById('mensaje').value;
+
+    let numero= 51999630629;
+    var win = window.open(`https://wa.me/${numero}?text=Hola%20mi%20nombre%20es%20${nombre}
+    , Asunto:%20${mensaje}`,'_blank');
+}
+evento.addEventListener('click',enviarFormulario)
